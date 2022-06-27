a5c4567ee13d3e58b5ea91995892c4b690d09470
mgreif <mika.greif@googlemail.com>
2022-06-27 16:43:54 +0200

    Change typos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,10 +13,10 @@ function App(props: AppProps) {
       <Head>
         <title>Carty | Clever Shopping List</title>
         <meta name="name" content="Carty | Clever Shopping List" />
-        <meta http-equiv="content-Type" content="text/html; utf-8" />
-        <meta http-equiv="Pragma" content="no-cache" />
+        <meta httpEquiv="content-Type" content="text/html; utf-8" />
+        <meta httpEquiv="Pragma" content="no-cache" />
         <meta name="robots" content="INDEX,NOFOLLOW" />
-        <meta http-equiv="content-Language" content="en" />
+        <meta httpEquiv="content-Language" content="en" />
         <meta name="description" content="Free clever shopping lists for social events (parties, conventions, etc..)" />
         <meta name="keywords" content="Free no-signup Shopping lists clever autofill social events online on the fly shopping list share" />
         <meta name="author" content="Mika Greif" />
@@ -25,7 +25,7 @@ function App(props: AppProps) {
         <meta name="audience" content="Alle" />
         <meta name="page-type" content="List, Persistant, Teilbar, Shareable" />
         <meta name="page-topic" content="Shopping, Online, Lists" />
-        <meta http-equiv="Reply-to" content="Mika@greif.me" />
+        <meta httpEquiv="Reply-to" content="Mika@greif.me" />
         <meta name="expires" content="" />
         <meta name="revisit-after" content="15 days" />
         <link rel="icon" href="images/favicon.ico" />
