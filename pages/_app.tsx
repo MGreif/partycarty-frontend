26bc626dd9f44d03094af2980288c24b255ab3b7
mgreif <mika.greif@googlemail.com>
2022-06-27 15:03:00 +0200

    Add mantine theme support for nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,34 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Layout>
-    <Component {...pageProps} />
-  </Layout>
+
+function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Carty | Clever Shopping List</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MantineProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default App
