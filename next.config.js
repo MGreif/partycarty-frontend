9f7a8b090af8b2efaac8e3a2ad94ccba3cbf1f32
mgreif <mika.greif@googlemail.com>
2022-06-29 14:48:29 +0200

    CSP

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const ContentSecurityPolicy = `
   font-src 'self';  
   frame-src *.googlesyndication.com *.google.com  https://googleads.g.doubleclick.net;
   connect-src 'self' *.googlesyndication.com *.google-analytics.com;
-  img-src *.googlesyndication.com;
+  img-src 'self' *.googlesyndication.com;
 `
 
 const securityHeaders = [
